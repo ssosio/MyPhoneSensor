9705335dd4ae73570d3fb00c90ccbf7cc6dbd233
ssosio <cksl1213@naver.com>
2023-05-23 14:51:20 +0900

    sensor확인기능 완벽구현

--- app/src/main/java/com/example/myphonesensor/MainActivity.java
+++ app/src/main/java/com/example/myphonesensor/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.myphonesensor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvSensor = findViewById(R.id.tvSensor);
+        SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensorList = sm.getSensorList(Sensor.TYPE_ALL);
+
+        String sensorInfo = "센서의 개수:";
+        sensorInfo = sensorInfo + sensorList.size();
+        int i = 1;
+        for(Sensor s : sensorList) {
+            sensorInfo = sensorInfo + "\n" + i++ +"\nName: " + s.getName() +
+                    "\n해상도: " + s.getResolution() + "\n최대범위" + s.getMaximumRange();
+        }
+
+        tvSensor.setText(sensorInfo);
     }
 }
